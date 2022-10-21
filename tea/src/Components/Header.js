f28465ea003c2e5ca07c9dee54cc78bf4cf359fa
Sean Keane <seankeane93@gmail.com>
2022-10-21 11:45:13 -0700

    WIP developing styling for application

--- tea/src/Components/Header.js
+++ tea/src/Components/Header.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import Barrys_Gold from '..Components/'
+import Barrys_Gold from './img/Barrys_Gold.webp'
+import Barrys_Original from './img/Barrys_Original.webp'
+import Barrys_Decaf from './img/Barrys_Decaf.webp'
+
 
 function Header() {
   return (
     <div className='header'>
       <h2>Barry's Irish Tea</h2>
       <h4>Est 1901</h4>
-      {/* <img src="tea\src\Components\Barrys_Gold.webp" width="100" height="50" /> */}
+      <div className='header-images'>
+      <img style={{ width: "18%", height: "100%" }} src={Barrys_Gold} alt="Barrys-Gold" />
+      <img style={{ width: "18%", height: "100%" }} src={Barrys_Original} alt="Barrys-Original" />
+      <img style={{ width: "18%", height: "100%" }} src={Barrys_Decaf} alt="Barrys-Decaf" />
+      </div>
     </div>
   )
 }
