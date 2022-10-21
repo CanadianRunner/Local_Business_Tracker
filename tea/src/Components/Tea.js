6089569fbfc88f1d90d7f26fcf5b5dfd78197e3c
Sean Keane <seankeane93@gmail.com>
2022-10-20 19:50:49 -0700

    Added logic for buttons on TeaControl.  Site successfully builds and buttons appear

---
+++ tea/src/Components/Tea.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+function Tea(props) {
+  return (
+    <React.Fragment>
+      <div>Tea</div>
+    </React.Fragment>
+  )
+}
+
+Tea.propTypes = {}
+
+export default Tea
--- tea/src/Components/TeaControl.js
+++ tea/src/Components/TeaControl.js
@@ -1,23 +1,50 @@
 import React, { Component } from 'react';
 import TeaList from './TeaList';
+import NewTeaForm from './NewTeaForm';
 
 export class TeaControl extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
+      formIsVisible: false,
       teaList: []
     };
   }
 
+  handleChangeViewClick = () => {
+    return this.setState(prevState => ({
+      formIsVisible: !prevState.formIsVisible
+    }))
+  }
+
+  addTeaToList = (newTea) => {
+    const newTeaList = this.state.teaList.concat(newTea);
+    this.setState({
+      teaList: newTeaList
+    });
+  }
+
   // js logic
   render() {
+    let currentVisibleElement = null;
+    let buttonText = null;
+
+    if (this.state.formIsVisible === true) {
+      currentVisibleElement = <NewTeaForm onNewTeaCreation={this.addTeaToList} />
+      buttonText= 'Return to tea list';
+    } else {
+      currentVisibleElement = <TeaList 
+        teaList={this.state.teaList} />
+        buttonText= 'Add new Tea!';
+    }
     return (
       <React.Fragment>
         <div className='tea-control'>
           <h1>Barrys Terrific Tea's</h1>
           <hr />
-          <TeaList />
+          {currentVisibleElement}
+          <button onClick={this.handleChangeViewClick}>{buttonText}</button>
         </div>
       </React.Fragment>
     );
