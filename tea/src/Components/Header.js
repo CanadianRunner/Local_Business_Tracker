fefb9559bf3bba7528edc528625ba917ee89f1e1
Sean Keane <seankeane93@gmail.com>
2022-10-21 11:13:43 -0700

    Working on images/styling

--- tea/src/Components/Header.js
+++ tea/src/Components/Header.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Barrys_Gold from '..Components/'
 
 function Header() {
   return (
     <div className='header'>
       <h2>Barry's Irish Tea</h2>
       <h4>Est 1901</h4>
+      {/* <img src="tea\src\Components\Barrys_Gold.webp" width="100" height="50" /> */}
     </div>
   )
 }
--- tea/src/Components/TeaDetail.js
+++ tea/src/Components/TeaDetail.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types'
 function TeaDetail(props) {
   const { tea, onClickDelete, onClickIncrease, onClickDecrease } = props;
   const cupsAvailable = tea.cups === 0 ? "Out of stock" : tea.cups;
-  //disable buy button
   return (
     <React.Fragment>
       <div>
